36b43fbf2eeda961f58e0a1b8e573f7599cd6541
kaptsov <kaptsovaa@gmail.com>
2022-04-15 00:36:51 +0400

    show pokemons on mainpage.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,25 +1,28 @@
 from django.db import models
 
+
 class Pokemon(models.Model):
+
     name = models.TextField()
     picture = models.ImageField(blank=True, null=True)
 
     def __str__(self):
         return self.name
 
+
 class PokemonEntity(models.Model):
 
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
     latitude = models.FloatField()
     longitude = models.FloatField()
-    appeared_at =  models.DateTimeField(blank=True, null=True)
-    dissappeared_at = models.DateTimeField(blank=True, null=True)
-    level = models.SmallIntegerField(blank=True)
-    health = models.SmallIntegerField(blank=True)
-    strength = models.SmallIntegerField(blank=True)
-    defense = models.SmallIntegerField(blank=True)
-    stamina = models.SmallIntegerField(blank=True)
+    appeared_at = models.DateTimeField(default=None, null=True, blank=True)
+    dissappeared_at = models.DateTimeField(default=None, null=True, blank=True)
+    level = models.SmallIntegerField(null=True, blank=True)
+    health = models.SmallIntegerField(blank=True, null=True)
+    strength = models.SmallIntegerField(blank=True, null=True)
+    defense = models.SmallIntegerField(blank=True, null=True)
+    stamina = models.SmallIntegerField(blank=True, null=True)
 
     def __str__(self):
-        return f'{self.latitude} - {self.longitude}'
+        return f'{self.pokemon} - {self.latitude} - {self.longitude}'
 
