915438dd459b94a985d5c12776bd5c122bf7bd3b
kaptsov <kaptsovaa@gmail.com>
2022-04-20 23:31:57 +0400

    Added element type

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 
+class PokemonElementType(models.Model):
+    title = models.CharField(verbose_name='Стихия', max_length=200, blank=True)
+
+    def __str__(self):
+        return f'{self.title}'
+
+
 class Pokemon(models.Model):
 
     name = models.TextField(verbose_name='Имя покемона')
@@ -8,6 +15,7 @@ class Pokemon(models.Model):
     title_en = models.CharField(verbose_name='Название (англ)', max_length=200, blank=True)
     title_jp = models.CharField(verbose_name='Название (яп)', max_length=200, blank=True)
     description = models.TextField(verbose_name='Описание', default='No description yet', blank=True)
+    pokemon_element_type = models.ManyToManyField(PokemonElementType)
     previous_evolution = models.ForeignKey(
         'self',
         on_delete=models.CASCADE,
@@ -27,13 +35,15 @@ class PokemonEntity(models.Model):
     latitude = models.FloatField(verbose_name='Широта')
     longitude = models.FloatField(verbose_name='Долгота')
     appeared_at = models.DateTimeField(verbose_name='Появился в ', default=None, null=True, blank=True)
-    dissappeared_at = models.DateTimeField(verbose_name = 'Исчез в ', default=None, null=True, blank=True)
-    level = models.SmallIntegerField(verbose_name = 'Уровень', null=True, blank=True)
-    health = models.SmallIntegerField(verbose_name = 'Здоровье', blank=True, null=True)
-    strength = models.SmallIntegerField(verbose_name = 'Сила', blank=True, null=True)
-    defense = models.SmallIntegerField(verbose_name = 'Защита', blank=True, null=True)
-    stamina = models.SmallIntegerField(verbose_name = 'Выносливость', blank=True, null=True)
+    dissappeared_at = models.DateTimeField(verbose_name='Исчез в ', default=None, null=True, blank=True)
+    level = models.SmallIntegerField(verbose_name='Уровень', null=True, blank=True)
+    health = models.SmallIntegerField(verbose_name='Здоровье', blank=True, null=True)
+    strength = models.SmallIntegerField(verbose_name='Сила', blank=True, null=True)
+    defense = models.SmallIntegerField(verbose_name='Защита', blank=True, null=True)
+    stamina = models.SmallIntegerField(verbose_name='Выносливость', blank=True, null=True)
 
     def __str__(self):
         return f'{self.pokemon} - {self.level}'
+
+
 
